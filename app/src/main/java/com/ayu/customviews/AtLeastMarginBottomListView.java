dead8ea21c77eaae03bb5c1ff25cf45e0778f5b6
tianyuanisme <tianyuanisme@gmail.com>
2016-10-13 23:27:02 +0800

    1. 上下边距由参考screen高度，改为参考父控件高度 2. 增加最小上边距的属性

--- app/src/main/java/com/ayu/customviews/AtLeastMarginBottomListView.java
+++ app/src/main/java/com/ayu/customviews/AtLeastMarginBottomListView.java
@@ -5,22 +5,28 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 public class AtLeastMarginBottomListView extends ListView {
     private float atLeastMarginBottom;
+    private float atLeastMarginTop;
+    private int parentHeight;
 
     public AtLeastMarginBottomListView(Context context, AttributeSet attrs) {
         super(context, attrs);
         TypedArray typedArray = getContext().obtainStyledAttributes(attrs, com.ayu.customviews.R.styleable.AtLeastMarginBottomListView);
         atLeastMarginBottom = typedArray.getDimension(com.ayu.customviews.R.styleable.AtLeastMarginBottomListView_atLeastMarginBottom, 0);
+        atLeastMarginTop = typedArray.getDimension(com.ayu.customviews.R.styleable.AtLeastMarginBottomListView_atLeastMarginTop, 0);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        Rect rect = new Rect();
-        getWindowVisibleDisplayFrame(rect);
-        int listViewHeight = (int) (rect.height() - atLeastMarginBottom);
+        parentHeight = ((ViewGroup) this.getParent()).getMeasuredHeight();
+        int listViewHeight = (int) (parentHeight - atLeastMarginBottom - atLeastMarginTop);
         if (listViewHeight > -1) {
             heightMeasureSpec = MeasureSpec.makeMeasureSpec(listViewHeight,
                     MeasureSpec.AT_MOST);
